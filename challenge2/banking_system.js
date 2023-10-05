509bb3636d5ab7b6ee561ee13fe6c3c337e5a563
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-10-06 00:25:01 +0700

    06/10/2023

--- challenge2/banking_system.js
+++ challenge2/banking_system.js
@@ -1,74 +1,86 @@
 class BankAccount {
-    constructor(saldo) {
+    constructor(params) {
+        if (this.constructor === BankAccount) {
+            throw new Error("Cannot Access");
+        }
+        let { saldo } = params;
         this.saldo = saldo;
     }
 
     deposit(angka) {
         setTimeout(() => {
-            try {
-                if (isNaN(angka)) {
-                    throw new Error(alert("Saldo harus berupa angka"));
-                }
-                if (angka <= 0) {
-                    throw new Error(alert("Saldo harus lebih besar dari 0"));
-                }
-                this.saldo += angka;
-                document.getElementById("saldo").textContent = this.saldo;
-                setTimeout(() => {
-                    alert(`Anda deposit sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
-                }, 2000);
-            } catch (error) {
-                console.error(error.message);
-            }
+            this.saldo += angka;
+            document.getElementById("saldo").textContent = this.saldo;
+            setTimeout(() => {
+                alert(`Anda deposit sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
+            }, 2000);
         }, 2000); // Simulate a 2-second delay
     }
 
     withdraw(angka) {
         setTimeout(() => {
-            try {
-                if (isNaN(angka)) {
-                    throw new Error(alert("Saldo harus berupa angka"));
-                }
-                if (angka <= 0) {
-                    throw new Error(alert("Saldo harus lebih besar dari 0"));
-                }
-                if (angka > this.saldo) {
-                    throw new Error(alert("Saldo tidak cukup"));
-                }
-                this.saldo -= angka;
-                document.getElementById("saldo").textContent = this.saldo;
-                setTimeout(() => {
-                    alert(`Anda withdraw sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
-                }, 2000);
-            } catch (error) {
-                console.error(error.message);
-            }
+            this.saldo -= angka;
+            document.getElementById("saldo").textContent = this.saldo;
+            setTimeout(() => {
+                alert(`Anda withdraw sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
+            }, 2000);
         }, 2000); // Simulate a 2-second delay
     }
 }
 
 class Proses extends BankAccount {
-    constructor(saldo) {
-        super(saldo);
+    constructor(params) {
+        super(params);
     }
 
     deposit(angka) {
-        super.deposit(angka);
+        try {
+            if (isNaN(angka)) {
+                throw new Error(alert("Saldo harus berupa angka"));
+            }
+            if (angka <= 0) {
+                throw new Error(alert("Saldo harus lebih besar dari 0"));
+            }
+            super.deposit(angka);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
     withdraw(angka) {
-        super.withdraw(angka);
+        try {
+            if (isNaN(angka)) {
+                throw new Error(alert("Saldo harus berupa angka"));
+            }
+            if (angka <= 0) {
+                throw new Error(alert("Saldo harus lebih besar dari 0"));
+            }
+            if (angka > this.saldo) {
+                throw new Error(alert(`Saldo tidak cukup untuk withdraw sebesar ${angka}`));
+            }
+            super.withdraw(angka);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 }
 
-const account = new Proses(0);
+const account = new Proses({saldo:0});
 
 function deposit() {
-    const depositInput = parseFloat(prompt("Masukkan jumlah saldo yang ingin ditambahkan"));
-    account.deposit(depositInput);
+    try {
+        const depositInput = parseFloat(prompt("Masukkan jumlah saldo yang ingin ditambahkan"));
+        account.deposit(depositInput);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 function withdraw() {
-    const withdrawInput = parseFloat(prompt("Masukkan jumlah saldo yang ingin dikurangi"));
-    account.withdraw(withdrawInput);
+    try {
+        const withdrawInput = parseFloat(prompt("Masukkan jumlah saldo yang ingin dikurangi"));
+        account.withdraw(withdrawInput);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
