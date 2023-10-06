7b1fd7e87b5daa230a105be9025f7074ce2ebfa6
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-10-06 18:01:23 +0700

    06/10/2023

--- challenge2/banking_system.js
+++ challenge2/banking_system.js
@@ -1,5 +1,6 @@
 class BankAccount {
     constructor(params) {
+        // implementasi Abstraction
         if (this.constructor === BankAccount) {
             throw new Error("Cannot Access");
         }
@@ -9,46 +10,51 @@ class BankAccount {
 
     deposit(angka) {
         setTimeout(() => {
-            this.saldo += angka;
-            document.getElementById("saldo").textContent = this.saldo;
-        }, 2000); // Simulate a 2-second delay
+            this.saldo += angka; // rumus
+            document.getElementById("saldo").textContent = this.saldo; // mengirim saldo setelah deposit
+        }, 2000); // setTimeout 2 detik
     }
 
     withdraw(angka) {
         setTimeout(() => {
-            this.saldo -= angka;
-            document.getElementById("saldo").textContent = this.saldo;
-        }, 2000); // Simulate a 2-second delay
+            this.saldo -= angka; // rumus
+            document.getElementById("saldo").textContent = this.saldo; // mengirim saldo setelah withdraw
+        }, 2000); // setTimeout 2 detik
     }
 
+    // implementasi private
     #private() {
         console.log("This is private");
     }
 
+    // implementasi protected
     _protected() {
         console.log("This is protected");
     }
 }
 
+// implementasi Polymorphism
+// class Alert
 const Alert = Base => class extends Base {
     deposit(angka) {
-        super.deposit(angka);
+        super.deposit(angka); // class BankAccount
         setTimeout(() => {
             alert(`Anda deposit sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
-        }, 4000);
+        }, 4000); // setTimeout 4 detik
     }
 
     withdraw(angka) {
-        super.withdraw(angka);
+        super.withdraw(angka); // class BankAccount
         setTimeout(() => {
             alert(`Anda withdraw sebesar ${angka}, Saldo anda saat ini sebesar ${this.saldo}`);
-        }, 4000);
+        }, 4000); // setTimeout 4 detik
     }
 }
 
+// Implementasi Polymorphism & Inheritance
 class TryCatch extends Alert(BankAccount) {
     constructor(params) {
-        super(params);
+        super(params); // class BankAccount
     }
 
     deposit(angka) {
@@ -59,7 +65,7 @@ class TryCatch extends Alert(BankAccount) {
             if (angka <= 0) {
                 throw new Error(alert("Saldo harus lebih besar dari 0"));
             }
-            super.deposit(angka);
+            super.deposit(angka); // class Alert
         } catch (error) {
             console.error(error.message);
         }
@@ -76,14 +82,14 @@ class TryCatch extends Alert(BankAccount) {
             if (angka > saldo) {
                 throw new Error(alert(`Saldo tidak cukup. Saldo saat ini sebesar ${saldo}`));
             }
-            super.withdraw(angka);
+            super.withdraw(angka); // class Alert
         } catch (error) {
             console.error(error.message);
         }
     }
 }
 
-const user = new TryCatch({ saldo: 0 });
+const user = new TryCatch({ saldo: 0 }); // deklarasi class TryCatch dengan parameter { saldo: 0 }
 
 function deposit() {
     try {
